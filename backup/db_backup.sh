dd5b045a5874dcce62ede299d891d25c30650e83
Eduard Saryan <eduard.saryan@protonmail.com>
2021-04-03 01:23:44 +0400

    Update paths

--- backup/db_backup.sh
+++ backup/db_backup.sh
@@ -9,4 +9,4 @@ now=$(date +"%d-%b-%Y")
 
 # Backup database.
 source /path/to/dockerized-jira8-mysql/.env.db
-docker exec -i $(docker ps -qf name=website-db) mysqldump --default-character-set=utf8mb4 -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} > /path/to/backup-folder/${MYSQL_DATABASE}_${now}.sql
+docker exec -i $(docker ps -qf name=webapp-db) mysqldump --default-character-set=utf8mb4 -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} > /path/to/backup-folder/${MYSQL_DATABASE}_${now}.sql
--- backup/db_restore.sh
+++ backup/db_restore.sh
@@ -2,4 +2,4 @@
 
 # Restore DB
 source /path/to/dockerized-jira8-mysql/.env.db
-cat /path/to/backup-file/${MYSQL_DATABASE}_some-date.sql | docker exec -i $(docker ps -qf name=jira-db) /usr/bin/mysql -u website_user --default-character-set=utf8mb4 -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE}
+cat /path/to/backup-file/${MYSQL_DATABASE}_some-date.sql | docker exec -i $(docker ps -qf name=jira-db) /usr/bin/mysql -u webapp_user --default-character-set=utf8mb4 -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE}
